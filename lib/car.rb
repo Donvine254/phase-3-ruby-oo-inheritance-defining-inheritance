5bf008116601518c32031e8ff6222b9ccfa7d7a2
donvine mugendi <donvinemugendi@gmail.com>
2023-07-06 00:47:37 +0300

    wow, a car!

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,9 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+    puts "Wow, a brand new car!"
+    def go
+        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    end
+end
